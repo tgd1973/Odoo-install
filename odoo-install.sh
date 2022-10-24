0bd2437186a034a4d81abe2c371760308c4e3683
tgd1973 <tgd1973@yahoo.com>
2022-10-24 15:02:58 +0200

    Update odoo-install.sh

--- odoo-install.sh
+++ odoo-install.sh
@@ -14,7 +14,7 @@
 ################################################################################
 
 sudo apt-get install git -y
-cd /tmp
-sudo git clone https://github.com/tgd1973/Odoo-install/install
-cd /tmp/install
+cd /tmp/
+sudo git clone https://github.com/tgd1973/Odoo-install/
+cd /tmp/Odoo-install/install/
 sudo chmod +x run.sh && sudo ./run.sh
