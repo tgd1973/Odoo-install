9d2da758cb31e3d8c5142a122da55f63268fca3d
tgd1973 <tgd1973@yahoo.com>
2022-10-24 16:39:01 +0200

    Update odoo-install.sh

--- odoo-install.sh
+++ odoo-install.sh
@@ -1,16 +1,15 @@
 #!/bin/bash
 ################################################################################
-# Script for installing Odoo on Ubuntu 16.04, 18.04 and 20.04 (could be used for other version too)
+# Script for installing Odoo on Ubuntu 16.04, 18.04 and 20.04 
+# (could be used for other version too)
 #-------------------------------------------------------------------------------
-# This script will install Odoo on your Ubuntu server. It can install multiple Odoo instances
-# in one Ubuntu because of the different xmlrpc_ports
+# This script will install Odoo on your Ubuntu server. It can install 
+# multiple Odoo instances in one Ubuntu because of the different xmlrpc_ports
 #-------------------------------------------------------------------------------
-# Make a new file:
-# sudo nano odoo-install.sh
-# Place this content in it and then make the file executable:
+# Run with command:
 # sudo chmod +x odoo-install.sh
 # Execute the script to install Odoo:
-# ./odoo-install
+# sudo ./odoo-install.sh
 ################################################################################
 
 sudo apt-get install git -y
