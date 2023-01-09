96c88879165ba749c284938110a6af959d26b717
tgd1973 <tgd1973@yahoo.com>
2023-01-09 18:34:36 +0100

    Update odoo-install.sh

--- odoo-install.sh
+++ odoo-install.sh
@@ -23,7 +23,7 @@ echo "
 ------------------------------------
 "
 while true; do
-        echo "If you want to use the default settings with Odoo-v15 & PostgreSQL-v14 continue."
+        echo "If you want to use the default settings with Odoo-v16 & PostgreSQL-v14 continue."
         read -p 'If you want to make changes, stop here and reload install. Continue ? (y/n)' yn
         case $yn in
         [Yy]*) break ;;
