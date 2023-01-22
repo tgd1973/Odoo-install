c4847855d37a8d739cf5761cf02c4671cde48ab8
tgd1973 <tgd1973@yahoo.com>
2023-01-22 11:26:08 +0100

    Update conf.sh

--- install/conf.sh
+++ install/conf.sh
@@ -2,40 +2,41 @@
 ##fixed parameters
 #odoo
 
-OE_VERSION="16.0"  ##|
-OS_VERSREPO="False" #| #Set TRUE to odoo14 if you want to install odoo14 from REPO
+OE_VERSION="16.0"  ##|_______________________________________________________________________________
+OS_VERSREPO="False" #| #Set TRUE for odoo14 if you want to install odoo14 from REPO _________________
 OE_INSTALL_DIR="$OE_HOME/$OE_VERSION"
 OE_REPO="$OE_INSTALL_DIR/odoo"
 #Set to true if you want to install it, false if you don't need it or have it already installed.
-INSTALL_WKHTMLTOPDF="True"
+INSTALL_WKHTMLTOPDF="True" #_________________________________________________________________________
 OE_PORT="8069"
 OE_NETRPC_PORT="8070"
 OE_LONGPOOL_PORT="8072"
 OE_WORKERS="4"
-#Choose the Odoo version which you want to install. For example: 9.0, 8.0, 7.0 or saas-6. When using 'trunk' the master version will be installed.
+#Choose the Odoo version which you want to install. For example: 9.0, 8.0, 7.0 or saas-6. 
+#When using 'trunk' the master version will be installed.
 #IMPORTANT! This script contains extra libraries that are specifically needed for Odoo 9.0
 
 # Set this to True if you want to install Odoo Enterprise!
-IS_ENTERPRISE="False"
+IS_ENTERPRISE="False" # _____________________________________________________________________________
 #set the superadmin password
-OE_SUPERADMIN="admin"
+OE_SUPERADMIN="admin" # _____________________________________________________________________________
 
-INSTALL_PG_SERVER="True" # if false, than only client will be installed
+INSTALL_PG_SERVER="True" # if false, than only client will be installed______________________________
 OE_DB_HOST="localhost"
 OE_DB_PORT="5432"
 OE_DB_USER="odoo"
 OE_DB_PASSWORD="odoo"
 PG_VERSION=14
 
-WEB_SERVER="nginx" # or "apache2" or "False" if false, the proxy does not install
+WEB_SERVER="nginx" # or "apache2" or "False" if false, the proxy does not install____________________
 
 HTTP_PROTOCOL="https"
 HTTPS_PORT="443"
-INSTALL_CERTIFICATE="false"
+INSTALL_CERTIFICATE="false" #________________________________________________________________________
 PUBLIC_IP="" # SET MANUALLY
 DOMAIN_NAME="_" # DNS SHOULD BE ALREADY CONFIGURED!
 DOMAIN_ALIASES=() # ("www.demo.ventortech.com" "zzz.demo.ventortech.com")
-LE_EMAIL="@mail"
+LE_EMAIL="@mail" #
 LE_CRON_SCRIPT="/etc/cron.daily/certbot-renew"
 
 if [ $IS_ENTERPRISE = "True" ]; then
